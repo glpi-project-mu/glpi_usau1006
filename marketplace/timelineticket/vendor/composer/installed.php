3208253fbf8ab8bfcbef2f9b8456886db8b096d7
CarlosIzarra09 <eduizarra2@hotmail.com>
2023-03-30 11:54:32 -0500

    remove .gitignore in timelineticket

---
+++ marketplace/timelineticket/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'a258e5c9565921c31576fd0423deea03003b1559',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'a258e5c9565921c31576fd0423deea03003b1559',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'szymach/c-pchart' => array(
+            'pretty_version' => 'v3.0.10',
+            'version' => '3.0.10.0',
+            'reference' => 'daf0912e0bd9069508a87a4419e4735a34e09698',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../szymach/c-pchart',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
