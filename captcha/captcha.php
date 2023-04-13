7a97d281a32ed7fbd703d1899292f3ca8f1a40ff
CarlosIzarra09 <eduizarra2@hotmail.com>
2023-04-13 15:22:06 -0500

    feat: modify style of captcha

--- captcha/captcha.php
+++ captcha/captcha.php
@@ -9,7 +9,7 @@ function generate_string($input, $strength = 10) {
     return $random_string;
 }
 function getImageCaptcha()  {
-  $image = imagecreatetruecolor(240, 50);
+  $image = imagecreatetruecolor(200, 50);
 
   imageantialias($image, true);
 
@@ -23,28 +23,44 @@ function getImageCaptcha()  {
     $colors[] = imagecolorallocate($image, $red - 20*$i, $green - 20*$i, $blue - 20*$i);
   }
 
-  imagefill($image, 0, 0, $colors[0]);
+  imagefill($image, 0, 0, -1);
 
-  for($i = 0; $i < 10; $i++) {
+  /*for($i = 0; $i < 10; $i++) {
     imagesetthickness($image, rand(2, 10));
     $line_color = $colors[rand(1, 4)];
     imagerectangle($image, rand(-10, 190), rand(-10, 10), rand(-10, 190), rand(40, 60), $line_color);
-  }
+  }*/
+
+  
+
+  
 
   $black = imagecolorallocate($image, 0, 0, 0);
   $white = imagecolorallocate($image, 255, 255, 255);
+  $blue = imagecolorallocate($image, 49, 68, 123);
   $textcolors = [$black, $white];
+
+  for ($i = 0; $i < 18; $i++) {
+    // Coordenadas de inicio y fin de la línea
+    $x1 = rand(0, imagesx($image));
+    $y1 = rand(0, imagesy($image));
+    $x2 = rand(0, imagesx($image));
+    $y2 = rand(0, imagesy($image));
+    
+    // Dibuja la línea en la imagen
+    imageline($image, $x1, $y1, $x2, $y2, $blue);
+}
 
   $fonts = [__DIR__.'/fonts/Acme.ttf', __DIR__.'/fonts/Ubuntu.ttf', __DIR__.'/fonts/Merriweather.ttf', __DIR__.'/fonts/Roboto.ttf'];
 
   $string_length = 6;
-  $captcha_string = generate_string('ABCDEFGHJKLMNPQRSTUVWXYZ1234567890abcdefghijklmnopqrstuvwxyz', $string_length);
+  $captcha_string = generate_string('ABCDEFGHJKLMNPQRSTUVWXYZ1234567890', $string_length);
 
   for($i = 0; $i < $string_length; $i++) {
-    $letter_space = 170/$string_length;
+    $letter_space = 120/$string_length;
     $initial = 50;
     
-    imagettftext($image, 24, rand(-15, 15), $initial + $i*$letter_space, rand(25, 45), $textcolors[rand(0, 1)], $fonts[array_rand($fonts)], $captcha_string[$i]);
+    imagettftext($image, 24, rand(-15, 15), $initial + $i*$letter_space, rand(25, 45), $blue, $fonts[array_rand($fonts)], $captcha_string[$i]);
   }
 
  
