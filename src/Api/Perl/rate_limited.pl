6a2d9efe61ff8bb67daec847f5debb912bc829ba
nivel3_esp04 <nivel3_esp04@minedu.gob.pe>
2024-06-10 21:03:57 -0500

    feat: Add local libs redis cpanm

--- src/Api/Perl/rate_limited.pl
+++ src/Api/Perl/rate_limited.pl
@@ -2,14 +2,16 @@
 use strict;
 use warnings;
 
-use lib 'src/Api/Perl';
+#use lib 'src/Api/Perl';
+use FindBin;
+use lib "$FindBin::Bin/local/lib/perl5";
 use RateLimiter;
 
 my $clientIp = $ARGV[0];
 
 my $obj = RateLimiter->new(
     server => '127.0.0.1:6379',
-    maxrate => 100,
+    maxrate => 10,
     maxrate_period => 60,
     namespace => 'rate_limit'
 );
